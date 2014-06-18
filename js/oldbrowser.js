8d170789787ef04d5a3a8658aced3e0c6b1ec8ca
Triangle717 <le717.code@yahoo.com>
2014-06-17 21:04:21 -0400

    Update old browser check

--- js/oldbrowser.js
+++ js/oldbrowser.js
@@ -1,4 +1,4 @@
-/*
+/**
  * LEGO Universe News! Minifig Wizard
  *
  * Created 2013-2014 Triangle717 & rioforce
@@ -9,26 +9,27 @@
  * <http://opensource.org/licenses/MIT>
  */
 
+
+/**
+ * Check for and stop displaying the Minifig Wizard
+ * on old browsers (mainly Internet Explorer)
+ */
 $(function() {
   "use strict";
-  /*
-   * Check for and stop displaying the Minifig Wizard
-   * on old browsers (mainly Internet Explorer)
-   */
 
   // Error message that might need to be displayed
-  /* jshint ignore:start */
-  var errorMessageText = '<style>#error-link{text-decoration:none;}#error-link:hover{text-decoration:underline;}span{font-style:italic;}</style>\n' +
-      '<p id="error">Your browser is not supported!<br>Please update your browser to enjoy the<br><span>LEGO Universe News!</span> Minifig Wizard.' +
-      '<br><br>You can research newer browsers at <a id="error-link" target="_blank" href="http://browsehappy.com/">browsehappy.com</a></p>',
-      /* jshint ignore:end */
+  var errorStyle = "#error-link{text-decoration:none;}#error-link:hover{text-decoration:underline;}span{font-style:italic;}",
+      errorText  = "<p class='error'>Your browser is not supported!<br>Please update your browser to enjoy the" +
+      "<br><span>LEGO Universe News!</span> Minifig Wizard.</p><p class='error'>You can research newer browsers at" +
+      "&nbsp;<a id='error-link' target='_blank' href='http://browsehappy.com/'>browsehappy.com</a></p>",
 
-  // I think the variable name says enough. :P
-  errorMessageCSS = {
-    "font-size": "200%",
+  // Error message styling
+  errorCSS       = {
+    "font-size": "1.9em",
     "font-weight": "bold",
+    "font-family": "Times New Roman, serif",
     "color": "red",
-    "margin-left": "40px"
+    "margin-left": "0.9em"
   };
 
   // Check if the browser does not support the border-radius CSS property
@@ -36,11 +37,15 @@ $(function() {
     // Remove page contents for a clean palette
     $("body").remove();
 
-    // Display error message
-    $("html").html(errorMessageText);
-
-    // Apply the CSS
-    $("#error").css(errorMessageCSS);
+    // Display error message and apply CSS
+    $("html").html("<head />");
+    $("head").append("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+    $("head").append("<style />");
+    $("head").append("<style />");
+    $("style").html(errorStyle);
+    $("html").html("<body />");
+    $("body").append(errorText);
+    $(".error").css(errorCSS);
     return false;
   }
 });
