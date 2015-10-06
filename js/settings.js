de7947fb1dee02eee48aea18561986cb54848cc7
Caleb Ely <le717.code@yahoo.com>
2015-10-06 14:06:17 -0400

    Remove document.ready signal

--- js/settings.js
+++ js/settings.js
@@ -12,69 +12,67 @@
 
 (function($) {
   "use strict";
-  $(function() {
-    // Display the minifigure
-    var images = document.LUN.decodeQuery();
-    if (images) {
-      images.forEach(function(value) {
-        $(document.LUN.getVariable("imgShadow")).before("<img class='the-big-picture' src='" + value + "'>");
-      });
+  // Display the minifigure
+  var images = document.LUN.decodeQuery();
+  if (images) {
+    images.forEach(function(value) {
+      $(document.LUN.getVariable("imgShadow")).before("<img class='the-big-picture' src='" + value + "'>");
+    });
+  }
+
+  /**
+   * Settings panel displaying
+   */
+  var QsettingsPanel = document.querySelector("#settings-panel");
+  document.querySelector("#btn-gear").addEventListener("click", function() {
+    QsettingsPanel.classList.toggle("visible");
+  });
+
+  /**
+   * Image size changing
+   */
+  var $sizeInput = $("#input-size");
+  $sizeInput.on("input", function() {
+    var imageSize = parseInt($sizeInput.val());
+
+    // Keep it real
+    if (!isNaN(imageSize)) {
+
+      // Clamp the image size if needed
+      if (imageSize > 600) {
+        imageSize = 600;
+      } else if (imageSize < 0) {
+        imageSize = 0;
+      }
+
+      // Resize all images to the new size
+      $(".the-big-picture").width(imageSize);
+      $sizeInput.val(imageSize);
+    }
+  });
+
+
+  /**
+   * Change the new window background color.
+   */
+  var QcolorBox = document.querySelector("#settings-panel tr"),
+      Qbody = document.querySelector("body");
+
+  QcolorBox.addEventListener("click", function(e) {
+    // Activate only on the color boxes
+    if (!e.target.classList.contains("color-box")) {
+      return false;
     }
 
-    /**
-     * Settings panel displaying
-     */
-    var QsettingsPanel = document.querySelector("#settings-panel");
-    document.querySelector("#btn-gear").addEventListener("click", function() {
-      QsettingsPanel.classList.toggle("visible");
-    });
+    // Remove previous colors
+    if (/color-(?!box)/.test(Qbody.className)) {
+      var oldClass = Qbody.className.match(/color-\w+/);
+      Qbody.classList.remove(oldClass);
+    }
 
-    /**
-     * Image size changing
-     */
-    var $sizeInput = $("#input-size");
-    $sizeInput.on("input", function() {
-      var imageSize = parseInt($sizeInput.val());
-
-      // Keep it real
-      if (!isNaN(imageSize)) {
-
-        // Clamp the image size if needed
-        if (imageSize > 600) {
-          imageSize = 600;
-        } else if (imageSize < 0) {
-          imageSize = 0;
-        }
-
-        // Resize all images to the new size
-        $(".the-big-picture").width(imageSize);
-        $sizeInput.val(imageSize);
-      }
-    });
-
-
-    /**
-     * Change the new window background color.
-     */
-    var QcolorBox = document.querySelector("#settings-panel tr"),
-        Qbody = document.querySelector("body");
-
-    QcolorBox.addEventListener("click", function(e) {
-      // Activate only on the color boxes
-      if (!e.target.classList.contains("color-box")) {
-        return false;
-      }
-
-      // Remove previous colors
-      if (/color-(?!box)/.test(Qbody.className)) {
-        var oldClass = Qbody.className.match(/color-\w+/);
-        Qbody.classList.remove(oldClass);
-      }
-
-      // Change the background color
-      document.querySelector(".color-box.white-border").classList.remove("white-border");
-      e.target.classList.add("white-border");
-      Qbody.classList.add(e.target.classList[1]);
-    });
+    // Change the background color
+    document.querySelector(".color-box.white-border").classList.remove("white-border");
+    e.target.classList.add("white-border");
+    Qbody.classList.add(e.target.classList[1]);
   });
 })(jQuery);
